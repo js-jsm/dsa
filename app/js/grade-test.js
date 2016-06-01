870a2248c9726c377aa1dbf5913753da9d777d59
gomugom <power4ce@gmail.com>
2016-06-02 02:19:19 +0900

    단위테스트 update

--- app/js/grade-test.js
+++ app/js/grade-test.js
@@ -24,15 +24,22 @@ module.exports = (()=>{
        ========= */
     let grades;
 
-    // log : 화면상에 함수 내용을 그대로 출력
-    ut.log(_=>{
+    // ut.title( string )
+    // : 화면상에 문자열을 제목으로 출력
+    ut.title('성적 / 평균');
+
+    // ut.log( function )
+    // : 화면상에 함수 내용 출력.
+    // : 반환되는 결과값이 있을 경우 `value : 결과값` 출력
+    ut.log(function(){
         grades = new Grades(20, 40, 60, 80);
     });
 
-    // equal : 함수(첫번째 인자)와 출력값(두번째 인자)가 같은지 여부를 판단.
-    ut.equal(_=> grades.average(), 50);
+    // ut.equal( function , expectValue )
+    // : 함수(첫번째 인자)와 출력값(두번째 인자)가 같은지 여부를 판단.
+    ut.equal(function (){ return grades.average(); }, 50);
     ut.log(_=> {
-        grades.add(100).add(30).add(70).add(10)
+        grades.add(100).add(30).add(70).add(10);
     });
     ut.equal(_=> grades.average(), 51.35);
     ut.equal(_=> grades.average(), 51.25);
--- app/js/utils/unit-test.js
+++ app/js/utils/unit-test.js
@@ -1,11 +1,21 @@
 const resultDom = document.getElementById('testResult');
 
+const trim = (func)=>
+    func.toString()
+    .replace(/^(function)( {0,}\()( {0,}\))/, '')
+    .replace(/^(_=>)( {0,}\{|)|(\{)/, '')
+    .replace(/\}$/, '')
+    .replace(/\s{2,}|(return )/g, '\n')
+    .trim();
 module.exports = {
+    title(str){
+        resultDom.innerHTML += `<h1>[ ${str} ]</h1>`;
+    },
     equal(func, expect){
         let isEqual = func() === expect;
         resultDom.innerHTML += `
         <div class="test-wrap ${isEqual ? 'equal' : 'not-equal'}">
-            <pre class="func">${func}</pre>
+            <pre class="func">${trim(func)}</pre>
             <pre class="expect">${expect}</pre>
             <div class="result">${isEqual ? 'OK!' : 'NOT OK!!'}</div>
         </div>`;
@@ -14,7 +24,7 @@ module.exports = {
         let val = func();
         resultDom.innerHTML += `
         <div class="test-wrap">
-            <pre class="func">${func}</pre>
+            <pre class="func">${trim(func)}</pre>
             ${val !== undefined ? `<pre class="value">value : ${val}</pre>` : ''}
         </div>`;
     }
