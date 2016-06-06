a280a612fc2c187cc4dd125c6c566bdb357516b0
yikl1004 <yikl1004@gmail.com>
2016-06-06 19:39:00 +0900

    Create 1_천명기.js

---
+++ 02_array/1_천명기.js
@@ -0,0 +1,47 @@
+
+// ES6 below
+var Grades = function( point ){
+	this.points = [];
+	if ( point !== undefined || typeof point == 'number'  ) {
+		this.points.push(point);
+	}
+};
+
+Grades.prototype.add = function( point ){
+	this.points.push( point );
+	console.log(this.points);
+	return this;
+};
+
+Grades.prototype.average = function() {
+	var points = this.points,
+		sum = 0;
+	for ( var i = 0; i<points.length; i++ ) {
+		sum += points[i];
+	}
+	return console.log(sum/points.length);
+};
+
+//ES 6
+class Grades {
+	constructor( point ) {
+		this.points = [];
+		if ( point !== undefined || typeof point == 'number'  ) {
+			this.points.push(point);
+		}
+	}
+	add ( pointer ) {
+		this.points.push( point );
+		console.log(this.points);
+		return this;
+	}
+	average () {
+		var points = this.points,
+			sum = 0,
+			result = 0;
+		for ( var i = 0; i<points.length; i++ )
+			sum += points[i];
+		result = sum/points.length
+		return console.log( result );
+	}
+}
