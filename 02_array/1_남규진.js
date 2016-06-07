7c0c9600f7276c66e93ec56c85dd59db5296f6b1
kjnam <kyujnam@gmail.com>
2016-06-07 19:40:27 +0900

    modify 코드리뷰 지적사항 수정

--- 02_array/1_남규진.js
+++ 02_array/1_남규진.js
@@ -1,31 +1,32 @@
-function grades() {
-	this.dataSource = [];
-	this.add = add;
-	this.average = average;
+function Grade() {
+    this.dataSource = [];
+    this.add = add;
+    this.average = average;
 }
 
 function add(score) {
-	this.dataSource.push(score);
+    this.dataSource.push(score);
 }
 
 function average() {
-	var total = 0;
-	for (var i=0; i<this.dataSource.length; ++i) {
-		total += this.dataSource[i];
-	}
+    var total = 0;
+    for (var i = 0, end = this.dataSource.length; i < end; ++i) {
+        total += this.dataSource[i];
+    }
 
-	return total / this.dataSource.length;
+    return total / this.dataSource.length;
 }
 
+// min 보다 크거나 같고, max 보다 작거나 같은 값을 반환한다
 function randomRange(min, max) {
-  return Math.floor( (Math.random() * (max - min + 1)) + min );
+    return Math.floor((Math.random() * (max - min + 1)) + min);
 }
 
-var grades = new grades();
-for (var i=0; i<=20; i++) {
-	grades.add(randomRange(0, 100));
+var grade = new Grade();
+for (var i = 0; i <= 20; i++) {
+    grade.add(randomRange(0, 100));
 }
-console.log(grades.dataSource);
-console.log(grades
-			.average()
-			.toFixed(2));
+console.log(grade.dataSource);
+console.log(grade
+    .average()
+    .toFixed(2));
