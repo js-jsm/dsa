d858011c277fdc857046441b0acfabb55c32350b
alakoko <alakoko.kim@gmail.com>
2016-07-11 20:46:21 +0900

    2장 과제 제출

---
+++ 07_dictionary/2_김지혜.js
@@ -0,0 +1,35 @@
+ function Dictionary(){
+   this.add = add;
+   this.datastore = new Array();
+   this.showAll = showAll;
+ }
+
+
+ function add(key){
+   if(this.datastore[key]){
+      this.datastore[key] += 1;
+    }else{
+      this.datastore[key] = 1;
+      }
+ }
+
+ function showAll(){
+   for each(var key in Object.keys(this.datastore)) {
+     console.log(key + ":" + this.datastore[key]);
+   }
+ }
+
+
+ var sentence = "the brown fox jumped over the blue fox.";
+ sentence = sentence.replace(/\./g,''); //문장의 끝 특수문자 '.' 대체
+ var voca = sentence.split(' '); //단어와 단어의 구분자 ' '(공백) 으로 split
+
+var countVoca = new Dictionary();
+
+//console.log(voca.length);
+
+for(var i=0;i<voca.length;i++){
+  countVoca.add(voca[i]);
+}
+
+countVoca.showAll();
