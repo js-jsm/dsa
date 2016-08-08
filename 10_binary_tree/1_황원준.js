80a581201d0350156168340414af505a44a253e6
hwangtan <hwangwonjun@gmail.com>
2016-08-08 21:22:19 +0900

    Update 1_황원준.js

--- 10_binary_tree/1_황원준.js
+++ 10_binary_tree/1_황원준.js
@@ -1,4 +1,4 @@
-//예제 풀이를 위한 코드 
+//예제1번 및 BST CLASS
 class Node {
     constructor(data, left, right) {
         this.data = data;
@@ -14,6 +14,8 @@ class Node {
 class BinarySearchTree {
     constructor() {
         this.root = null;
+        this.count = 0; //used exam1
+        this.edge = 0; //used exam1
     }
 
     insert(data) {
@@ -21,6 +23,7 @@ class BinarySearchTree {
 
         if(this.root === null) {
             this.root = newNode
+            ++this.count;
         } else {
             this.insertNode(this.root, newNode)
         }
@@ -30,28 +33,81 @@ class BinarySearchTree {
         if (newNode.data < node.data) {
             if (node.left === undefined) {
                 node.left = newNode;
+                ++this.count;
+                ++this.edge;
             } else {
                 this.insertNode(node.left, newNode);
             }
         } else {
             if (node.right === undefined) {
                 node.right = newNode;
+                ++this.count;
+                ++this.edge;
             } else {
                 this.insertNode(node.right, newNode);
             }
         }
     }
-
-  
-    //작동안됨
-     inOrder(node) {
+    /* 트리 순회 */
+    inOrder(node) {
         if(node !== undefined) {
             this.inOrder(node.left);
             console.log(node.show());
             this.inOrder(node.right);
         }
     }
- 
+    preOrder(node) {
+           if(node !== undefined) {
+            console.log(node.show());
+            this.inOrder(node.left);
+            this.inOrder(node.right);
+        }
+    }
+
+    postOrder(node) {
+           if(node !== undefined) {
+            this.inOrder(node.left);
+            this.inOrder(node.right);
+            console.log(node.show());
+        }
+    }
+    /*노드 갯수 엣지 갯수 exam*/
+    getCount() {
+        return this.count;
+    }
+    
+    getEdge() {
+        return this.edge;
+    }
+
+    /*최대값 최소값 exam*/
+    min() {
+        return this.minNode(this.root);
+    }
+
+    minNode(node) {
+        if (node) {
+            while (node && node.left !== undefined) {
+                node = node.left
+            }
+            return node.data;
+        }
+        return null;
+    }   
+    
+    max() {
+        return this.maxNode(this.root);
+    }
+
+    maxNode(node) {
+        if (node) {
+            while (node && node.right !== undefined) {
+                node = node.right;
+            }
+            return node.data;
+        }
+        return null;
+    }
 
 }
 
@@ -72,3 +128,10 @@ tree.insert(18);
 tree.insert(25);
 
 tree.inOrder(tree.root);
+tree.preOrder(tree.root);
+tree.postOrder(tree.root);
+
+tree.min();
+tree.max();
+tree.getCount();
+tree.getEdge();
